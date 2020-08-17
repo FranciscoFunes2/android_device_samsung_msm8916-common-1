80b86f9e65ef33f5eb529908b3069f765de8e6de
itexpert120 <itexpert120@gmail.com>
2020-08-17 06:58:59 +0200

    Add some System.prop Tweaks
    Change-Id: Ie5901ab6a1343452bd888df479990621b9cab3c4

--- msm8916.mk
+++ msm8916.mk
@@ -433,6 +433,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true
 
+# System.prop tweaks
+TARGET_SYSTEM_PROP += \
+    device/samsung/msm8916-common/system.prop
+
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.rild.nitz_long_ons_0="" \
